e6bde42f384fea69f8422acf10ee0f097b5821d9
igrmm <igrmmdev@gmail.com>
2020-01-18 16:55:37 -0400

    rm badlogic.jpg; add player.png

--- core/src/com/igrmm/gdx2d/Gdx2D.java
+++ core/src/com/igrmm/gdx2d/Gdx2D.java
@@ -13,7 +13,7 @@ public class Gdx2D extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("player.png");
 	}
 
 	@Override
