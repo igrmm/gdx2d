4a7be660916df4678b1e16cfd5949770636545e8
igrmm <igrmmdev@gmail.com>
2020-01-18 17:04:59 -0400

    add GameObject class

---
+++ core/src/com/igrmm/gdx2d/GameObject.java
@@ -0,0 +1,42 @@
+package com.igrmm.gdx2d;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.ArrayList;
+
+public class GameObject {
+	public final Rectangle bounds;
+	public final Vector2 position;
+	private final Vector2 center;
+
+	public GameObject(Rectangle bounds) {
+		this.bounds = bounds;
+		position = new Vector2(bounds.x, bounds.y);
+		center = new Vector2();
+		bounds.getCenter(center);
+	}
+
+	public Vector2 getCenter() {
+		return bounds.getCenter(center);
+	}
+
+	public float getX() {
+		return bounds.getX();
+	}
+
+	public float getY() {
+		return bounds.getY();
+	}
+
+	public float getWidth() {
+		return bounds.getWidth();
+	}
+
+	public float getHeight() {
+		return bounds.getHeight();
+	}
+
+	public void update(float delta, ArrayList<GameObject> queue) {
+	}
+}
