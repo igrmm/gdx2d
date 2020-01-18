f2343bc88098c59986e0c51e75d3bb5a8948a480
igrmm <igrmmdev@gmail.com>
2020-01-18 10:31:54 -0400

    first commit - push project generated with gdx wizard

---
+++ android/src/com/igrmm/gdx2d/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.igrmm.gdx2d;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.igrmm.gdx2d.Gdx2D;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Gdx2D(), config);
+	}
+}
---
+++ desktop/src/com/igrmm/gdx2d/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.igrmm.gdx2d.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.igrmm.gdx2d.Gdx2D;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Gdx2D(), config);
+	}
+}
