876cac96c94de238f29c8b427e57bd1ac5c69c48
igrmm <igrmmdev@gmail.com>
2020-01-19 15:32:30 -0400

    add Waypoint class

---
+++ core/src/com/igrmm/gdx2d/entities/Waypoint.java
@@ -0,0 +1,9 @@
+package com.igrmm.gdx2d.entities;
+
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+
+public class Waypoint extends Entity {
+	public Waypoint(RectangleMapObject rectangleMapObject) {
+		super(rectangleMapObject);
+	}
+}
