ef29c06e21bf1634c283139235420a9431cad222
igrmm <igrmmdev@gmail.com>
2020-01-19 15:32:05 -0400

    add Block class

---
+++ core/src/com/igrmm/gdx2d/entities/Block.java
@@ -0,0 +1,9 @@
+package com.igrmm.gdx2d.entities;
+
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+
+public class Block extends Entity {
+	public Block(RectangleMapObject rectangleMapObject) {
+		super(rectangleMapObject);
+	}
+}
