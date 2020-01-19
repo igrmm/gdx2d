f6c707a09494d34302c094abffeac99a73d79bd5
igrmm <igrmmdev@gmail.com>
2020-01-19 15:38:52 -0400

    Set LoadScreen; create instance of Assets and Saves;

--- core/src/com/igrmm/gdx2d/Gdx2D.java
+++ core/src/com/igrmm/gdx2d/Gdx2D.java
@@ -1,33 +1,19 @@
 package com.igrmm.gdx2d;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import com.igrmm.gdx2d.screens.LoadScreen;
 
-public class Gdx2D extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class Gdx2D extends Game {
+	public final Assets assets = new Assets();
+	public final Saves saves = new Saves();
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("player.png");
+	public void create() {
+		setScreen(new LoadScreen(this));
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose() {
+		assets.dispose();
 	}
 }
