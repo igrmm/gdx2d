e7234aae895b7c85444945e1816d3f62d0b4255a
igrmm <igrmmdev@gmail.com>
2020-03-16 15:50:47 -0400

    add input component to player entity

--- core/src/com/igrmm/gdx2d/ecs/entities/Player.java
+++ core/src/com/igrmm/gdx2d/ecs/entities/Player.java
@@ -1,6 +1,7 @@
 package com.igrmm.gdx2d.ecs.entities;
 
 import com.igrmm.gdx2d.ecs.EntityManager;
+import com.igrmm.gdx2d.ecs.components.CInput;
 import com.igrmm.gdx2d.ecs.components.CPosition;
 import com.igrmm.gdx2d.ecs.components.CRigidBody;
 
@@ -9,6 +10,7 @@ public class Player extends Entity {
 		super(manager);
 		manager.cPositionHashMap.put(this.id, new CPosition());
 		manager.cRigidBodyHashMap.put(this.id, new CRigidBody());
+		manager.cInputHashMap.put(this.id, new CInput());
 		manager.entities.add(this);
 	}
 }
