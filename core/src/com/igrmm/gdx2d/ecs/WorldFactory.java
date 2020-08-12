2a293ae0d3e24de3f8a7f86445a21b8bb2eb9037
igrmm <igrmmdev@gmail.com>
2020-08-12 16:33:02 -0400

    initialize entities in WorldFactory

--- core/src/com/igrmm/gdx2d/ecs/WorldFactory.java
+++ core/src/com/igrmm/gdx2d/ecs/WorldFactory.java
@@ -3,8 +3,9 @@ package com.igrmm.gdx2d.ecs;
 import com.badlogic.gdx.maps.MapGroupLayer;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.igrmm.gdx2d.ecs.entities.Block;
+import com.igrmm.gdx2d.ecs.components.BoundingBoxComponent;
 
 public class WorldFactory {
     public static World make(TiledMap map) {
@@ -20,7 +21,10 @@ public class WorldFactory {
                         break;
 
                     case "block":
-                        Block block = new Block(world, mapObject);
+                        int blockId = world.getUniqueId();
+                        world.entities.add(blockId);
+                        RectangleMapObject rectangleMapObject = (RectangleMapObject) mapObject;
+                        world.blockBoundingBoxes.put(blockId, new BoundingBoxComponent(rectangleMapObject.getRectangle()));
                         break;
 
                     default:
