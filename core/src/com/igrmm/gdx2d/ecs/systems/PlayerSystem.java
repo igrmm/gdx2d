49bd332196ba4211dcae05ecd0312a6f8ab6073d
igrmm <igrmmdev@gmail.com>
2020-08-12 16:30:33 -0400

    add PlayerSystem class

---
+++ core/src/com/igrmm/gdx2d/ecs/systems/PlayerSystem.java
@@ -0,0 +1,10 @@
+package com.igrmm.gdx2d.ecs.systems;
+
+import com.igrmm.gdx2d.ecs.World;
+
+public class PlayerSystem implements System {
+
+    @Override
+    public void update(World world) {
+    }
+}
