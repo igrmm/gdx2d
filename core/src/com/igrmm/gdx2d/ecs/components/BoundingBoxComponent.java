155ffa071eb74bc961b7b0e20c3d9eefa22b62b8
igrmm <igrmmdev@gmail.com>
2020-08-12 16:31:52 -0400

    rename: add Component suffix to components

---
+++ core/src/com/igrmm/gdx2d/ecs/components/BoundingBoxComponent.java
@@ -0,0 +1,16 @@
+package com.igrmm.gdx2d.ecs.components;
+
+import com.badlogic.gdx.math.Rectangle;
+
+public class BoundingBoxComponent extends Rectangle implements Component {
+    public BoundingBoxComponent(float x, float y, float width, float height) {
+        super(x, y, width, height);
+    }
+
+    public BoundingBoxComponent(Rectangle rect) {
+        super(rect);
+    }
+
+    public BoundingBoxComponent() {
+    }
+}
