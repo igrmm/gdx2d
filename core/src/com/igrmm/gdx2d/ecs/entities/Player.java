474e06fcfa5a03b7b6b0068eab0dfe24f6a5850b
igrmm <igrmmdev@gmail.com>
2020-08-11 21:30:31 -0400

    change player position

--- core/src/com/igrmm/gdx2d/ecs/entities/Player.java
+++ core/src/com/igrmm/gdx2d/ecs/entities/Player.java
@@ -1,10 +1,12 @@
 package com.igrmm.gdx2d.ecs.entities;
 
-import com.igrmm.gdx2d.ecs.EntityManager;
+import com.igrmm.gdx2d.ecs.World;
+import com.igrmm.gdx2d.ecs.components.BoundingBox;
 
 public class Player extends Entity {
-	public Player(EntityManager manager) {
-		super(manager);
-		manager.entities.add(this);
-	}
+    public Player(World world) {
+        super(world);
+
+        world.playerBoundingBox = new BoundingBox(100, 100, 32, 32);
+    }
 }
