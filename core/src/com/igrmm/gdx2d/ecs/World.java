65b5bc3a387a42861a481a127f714d8a919dc674
igrmm <igrmmdev@gmail.com>
2020-08-12 21:19:16 -0400

    move player around using InputComponent and PlayerSystem

--- core/src/com/igrmm/gdx2d/ecs/World.java
+++ core/src/com/igrmm/gdx2d/ecs/World.java
@@ -1,6 +1,8 @@
 package com.igrmm.gdx2d.ecs;
 
 import com.igrmm.gdx2d.ecs.components.BoundingBoxComponent;
+import com.igrmm.gdx2d.ecs.components.InputComponent;
+import com.igrmm.gdx2d.ecs.systems.PlayerSystem;
 import com.igrmm.gdx2d.ecs.systems.System;
 
 import java.util.HashMap;
@@ -20,15 +22,27 @@ public class World {
 
     /* PLAYER COMPONENTS*/
     public final BoundingBoxComponent playerBoundingBoxComponent;
+    public final InputComponent playerInputComponent = new InputComponent();
 
     public World() {
+
+        /* INITIALIZE PLAYER */
         int player = getUniqueId();
         playerBoundingBoxComponent = new BoundingBoxComponent(100, 100, 32, 32);
+
+        /* CREATE SYSTEMS */
+        systems.add(new PlayerSystem());
     }
 
     public int getUniqueId() {
         int uniqueId = idCounter;
         idCounter++;
         return uniqueId;
+    }
+
+    public void update(float delta) {
+        for (System system : systems) {
+            system.update(this);
+        }
     }
 }
--- core/src/com/igrmm/gdx2d/ecs/systems/PlayerSystem.java
+++ core/src/com/igrmm/gdx2d/ecs/systems/PlayerSystem.java
@@ -1,10 +1,39 @@
 package com.igrmm.gdx2d.ecs.systems;
 
 import com.igrmm.gdx2d.ecs.World;
+import com.igrmm.gdx2d.ecs.components.BoundingBoxComponent;
+import com.igrmm.gdx2d.ecs.components.InputComponent;
 
 public class PlayerSystem implements System {
 
     @Override
     public void update(World world) {
+        InputComponent inputs = world.playerInputComponent;
+        BoundingBoxComponent boundingBox = world.playerBoundingBoxComponent;
+
+        if (inputs.right) {
+            boundingBox.x += 5;
+        }
+
+        if (inputs.left) {
+            boundingBox.x -= 5;
+        }
+
+        if (inputs.up) {
+            boundingBox.y += 5;
+        }
+
+        if (inputs.down) {
+            boundingBox.y -= 5;
+        }
+
+        resetInputs(inputs);
+    }
+
+    private void resetInputs(InputComponent inputs) {
+        inputs.right = false;
+        inputs.left = false;
+        inputs.up = false;
+        inputs.down = false;
     }
 }
