bea85a9cc16ea222a5eec6deaace8a1167727f05
igrmm <igrmmdev@gmail.com>
2020-11-27 11:59:28 -0400

    rewrite PhysicsSystem to work with new collision class

--- core/src/com/igrmm/gdx2d/ecs/systems/PhysicsSystem.java
+++ core/src/com/igrmm/gdx2d/ecs/systems/PhysicsSystem.java
@@ -1,12 +1,14 @@
 package com.igrmm.gdx2d.ecs.systems;
 
-import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
+import com.igrmm.gdx2d.ecs.Collision;
 import com.igrmm.gdx2d.ecs.Components;
 import com.igrmm.gdx2d.ecs.components.BoundingBoxComponent;
+import com.igrmm.gdx2d.ecs.components.BroadPhaseCollisionComponent;
+import com.igrmm.gdx2d.ecs.components.VelocityComponent;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
 
 public class PhysicsSystem implements System {
 	private final ArrayList<BoundingBoxComponent> queue = new ArrayList<>();
@@ -14,71 +16,19 @@ public class PhysicsSystem implements System {
 
 	@Override
 	public void update(Components components) {
-		HashMap<String, BoundingBoxComponent> dynamicBBoxes = components.dynamicBoundingBoxComponents;
-		HashMap<String, BoundingBoxComponent> blockBBoxes = components.blockBoundingBoxComponents;
 
-		for (BoundingBoxComponent dynamicBBox : dynamicBBoxes.values()) {
-			for (BoundingBoxComponent blockBBox : blockBBoxes.values()) {
-				if (dynamicBBox.overlaps(blockBBox)) {
-					queue.add(blockBBox);
-				}
-			}
+		BroadPhaseCollisionComponent broadPhaseCollisionComponent = components.broadPhaseCollisionComponents.get(components.playerID);
+		List<Collision> collisions = broadPhaseCollisionComponent.collisions;
 
-			if (!queue.isEmpty()) {
-				for (BoundingBoxComponent blockBBox : queue) {
-					if (Intersector.intersectRectangles(dynamicBBox, blockBBox, intersection)) {
-
-						//upper left corner
-						if (dynamicBBox.x != intersection.x && dynamicBBox.y == intersection.y) {
-							if (intersection.width > intersection.height) {
-								dynamicBBox.y = intersection.y + intersection.height;
-							} else if (intersection.width < intersection.height) {
-								dynamicBBox.x = intersection.x - dynamicBBox.width;
-							} else if (intersection.width == intersection.height && queue.size() == 1) {
-								dynamicBBox.y = intersection.y + intersection.height;
-								//dynamicBBox.x = intersection.x - dynamicBBox.width;
-							}
-						}
-
-						//upper right corner
-						else if (dynamicBBox.x == intersection.x && dynamicBBox.y == intersection.y) {
-							if (intersection.width > intersection.height) {
-								dynamicBBox.y = intersection.y + intersection.height;
-							} else if (intersection.width < intersection.height) {
-								dynamicBBox.x = intersection.x + intersection.width;
-							} else if (intersection.width == intersection.height && queue.size() == 1) {
-								dynamicBBox.y = intersection.y + intersection.height;
-								//dynamicBBox.x = intersection.x + intersection.width;
-							}
-						}
-
-						//lower left corner
-						else if (dynamicBBox.x != intersection.x && dynamicBBox.y != intersection.y) {
-							if (intersection.width > intersection.height) {
-								dynamicBBox.y = intersection.y - dynamicBBox.height;
-							} else if (intersection.width < intersection.height) {
-								dynamicBBox.x = intersection.x - dynamicBBox.width;
-							} else if (intersection.width == intersection.height && queue.size() == 1) {
-								dynamicBBox.y = intersection.y - dynamicBBox.height;
-								//dynamicBBox.x = intersection.x - dynamicBBox.width;
-							}
-						}
-
-						//lower right corner
-						else if (dynamicBBox.x == intersection.x && dynamicBBox.y != intersection.y) {
-							if (intersection.width > intersection.height) {
-								dynamicBBox.y = intersection.y - dynamicBBox.height;
-							} else if (intersection.width < intersection.height) {
-								dynamicBBox.x = intersection.x + intersection.width;
-							} else if (intersection.width == intersection.height && queue.size() == 1) {
-								dynamicBBox.y = intersection.y - dynamicBBox.height;
-								//dynamicBBox.x = intersection.x + intersection.width;
-							}
-						}
-					}
-				}
-			}
-			queue.clear();
+		for (Collision collision : collisions) {
+			collision.resolve();
 		}
+		collisions.clear();
+
+		BoundingBoxComponent player = components.dynamicBoundingBoxComponents.get(components.playerID);
+		VelocityComponent velocityComponent = components.velocityComponents.get(components.playerID);
+		player.x += velocityComponent.velocity.x;
+		player.y += velocityComponent.velocity.y;
+		velocityComponent.velocity.set(0.0f, 0.0f);
 	}
 }
