6d66fee6cb96f87e45ddb3e4b31b777b00797f8a
igrmm <igrmmdev@gmail.com>
2020-11-27 11:57:37 -0400

    add BlockSystem for collision resolving with blocks

---
+++ core/src/com/igrmm/gdx2d/ecs/systems/BlockSystem.java
@@ -0,0 +1,31 @@
+package com.igrmm.gdx2d.ecs.systems;
+
+import com.igrmm.gdx2d.ecs.Collision;
+import com.igrmm.gdx2d.ecs.Components;
+import com.igrmm.gdx2d.ecs.components.BoundingBoxComponent;
+import com.igrmm.gdx2d.ecs.components.BroadPhaseCollisionComponent;
+import com.igrmm.gdx2d.ecs.components.VelocityComponent;
+
+import java.util.HashMap;
+
+public class BlockSystem implements System {
+	@Override
+	public void update(Components components) {
+
+		HashMap<String, BoundingBoxComponent> blocks = components.blockBoundingBoxComponents;
+
+		//TEMP PLAYER CODE
+		BoundingBoxComponent player = components.dynamicBoundingBoxComponents.get(components.playerID);
+		VelocityComponent velocityComponent = components.velocityComponents.get(components.playerID);
+		BroadPhaseCollisionComponent broadPhaseCollisionComponent = components.broadPhaseCollisionComponents.get(components.playerID);
+		for (BoundingBoxComponent blockBBox : blocks.values()) {
+			Collision collision = new Collision(player, blockBBox, velocityComponent.velocity) {
+				@Override
+				public boolean resolve() {
+					return super.resolve();
+				}
+			};
+			broadPhaseCollisionComponent.collisions.add(collision);
+		}
+	}
+}
