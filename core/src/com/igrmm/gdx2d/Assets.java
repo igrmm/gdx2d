356e5eb7d99d0121953598df76021e787e06649d
igrmm <igrmmdev@gmail.com>
2020-11-27 17:03:50 -0400

    add loadMaps() for loading every map using enum

--- core/src/com/igrmm/gdx2d/Assets.java
+++ core/src/com/igrmm/gdx2d/Assets.java
@@ -4,9 +4,16 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.igrmm.gdx2d.assets.Map;
 
 public class Assets extends AssetManager {
 	public Assets() {
 		setLoader(TiledMap.class, new TmxMapLoader(new InternalFileHandleResolver()));
+	}
+
+	public void loadMaps() {
+		for (Map map : Map.values()) {
+			load(map.getPath(), TiledMap.class);
+		}
 	}
 }
