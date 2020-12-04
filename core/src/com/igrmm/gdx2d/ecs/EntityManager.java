d427dbbd0d02beaa8c1356b2df26c63647fb21a9
igrmm <igrmmdev@gmail.com>
2020-12-04 15:53:01 -0400

    add entities (set of strings) and default constructor

--- core/src/com/igrmm/gdx2d/ecs/EntityManager.java
+++ core/src/com/igrmm/gdx2d/ecs/EntityManager.java
@@ -1,4 +1,12 @@
 package com.igrmm.gdx2d.ecs;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class EntityManager {
+	public final Set<String> entities;
+
+	public EntityManager() {
+		entities = new HashSet<>();
+	}
 }
