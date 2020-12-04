c89488805d4379d8e8e5cc257822974cd8e5a155
igrmm <igrmmdev@gmail.com>
2020-12-04 16:01:14 -0400

    add map of generic components

--- core/src/com/igrmm/gdx2d/ecs/EntityManager.java
+++ core/src/com/igrmm/gdx2d/ecs/EntityManager.java
@@ -1,12 +1,18 @@
 package com.igrmm.gdx2d.ecs;
 
+import com.igrmm.gdx2d.ecs.components.Component;
+
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class EntityManager {
 	public final Set<String> entities;
+	public final Map<Class<?>, Map<String, ? extends Component>> components;
 
 	public EntityManager() {
 		entities = new HashSet<>();
+		components = new HashMap<>();
 	}
 }
