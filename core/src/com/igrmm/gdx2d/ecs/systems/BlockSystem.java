92959f26b33dad09b003e085b2f9670c2d494ffd
igrmm <igrmmdev@gmail.com>
2021-01-05 14:46:09 -0400

    make BlockSystem compatible with new entity component system

--- core/src/com/igrmm/gdx2d/ecs/systems/BlockSystem.java
+++ core/src/com/igrmm/gdx2d/ecs/systems/BlockSystem.java
@@ -6,19 +6,19 @@ import com.igrmm.gdx2d.ecs.components.BoundingBoxComponent;
 import com.igrmm.gdx2d.ecs.components.BroadPhaseCollisionComponent;
 import com.igrmm.gdx2d.ecs.components.VelocityComponent;
 
-import java.util.HashMap;
+import java.util.List;
 
 public class BlockSystem implements System {
 	@Override
 	public void update(EntityManager entityManager) {
 
-		HashMap<String, BoundingBoxComponent> blocks = components.blockBoundingBoxComponents;
+		List<BoundingBoxComponent> blocks = entityManager.getAllComponentsOfType(BoundingBoxComponent.class);
 
 		//TEMP PLAYER CODE
-		BoundingBoxComponent player = components.dynamicBoundingBoxComponents.get(components.playerID);
-		VelocityComponent velocityComponent = components.velocityComponents.get(components.playerID);
-		BroadPhaseCollisionComponent broadPhaseCollisionComponent = components.broadPhaseCollisionComponents.get(components.playerID);
-		for (BoundingBoxComponent blockBBox : blocks.values()) {
+		BoundingBoxComponent player = entityManager.getComponent(entityManager.playerUUID, BoundingBoxComponent.class);
+		VelocityComponent velocityComponent = entityManager.getComponent(entityManager.playerUUID, VelocityComponent.class);
+		BroadPhaseCollisionComponent broadPhaseCollisionComponent = entityManager.getComponent(entityManager.playerUUID, BroadPhaseCollisionComponent.class);
+		for (BoundingBoxComponent blockBBox : blocks) {
 			Collision collision = new Collision(player, blockBBox, velocityComponent.velocity) {
 				@Override
 				public boolean resolve() {
