818128b3e61e1cf2c1768b7a308fde1e01db023a
igrmm <igrmmdev@gmail.com>
2021-01-19 14:22:27 -0400

    add AccelerationComponent

---
+++ core/src/com/igrmm/gdx2d/ecs/components/AccelerationComponent.java
@@ -0,0 +1,7 @@
+package com.igrmm.gdx2d.ecs.components;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class AccelerationComponent implements Component {
+	public Vector2 acceleration = new Vector2();
+}
