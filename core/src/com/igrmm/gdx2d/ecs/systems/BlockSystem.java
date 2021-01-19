5f5a1a5b27c949a0b2891f355429f14605420b5e
igrmm <igrmmdev@gmail.com>
2021-01-19 15:01:29 -0400

    make BlockSystem compatible with new add GravitySystem

--- core/src/com/igrmm/gdx2d/ecs/systems/BlockSystem.java
+++ core/src/com/igrmm/gdx2d/ecs/systems/BlockSystem.java
@@ -4,6 +4,7 @@ import com.igrmm.gdx2d.ecs.Collision;
 import com.igrmm.gdx2d.ecs.EntityManager;
 import com.igrmm.gdx2d.ecs.components.BoundingBoxComponent;
 import com.igrmm.gdx2d.ecs.components.BroadPhaseCollisionComponent;
+import com.igrmm.gdx2d.ecs.components.JumpComponent;
 import com.igrmm.gdx2d.ecs.components.VelocityComponent;
 
 import java.util.List;
@@ -17,12 +18,17 @@ public class BlockSystem implements System {
 		//TEMP PLAYER CODE
 		BoundingBoxComponent player = entityManager.getComponent(entityManager.playerUUID, BoundingBoxComponent.class);
 		VelocityComponent velocityComponent = entityManager.getComponent(entityManager.playerUUID, VelocityComponent.class);
+		JumpComponent jumpComponent = entityManager.getComponent(entityManager.playerUUID, JumpComponent.class);
+		jumpComponent.grounded = false;
 		BroadPhaseCollisionComponent broadPhaseCollisionComponent = entityManager.getComponent(entityManager.playerUUID, BroadPhaseCollisionComponent.class);
 		for (BoundingBoxComponent blockBBox : blocks) {
 			Collision collision = new Collision(player, blockBBox, velocityComponent.velocity) {
 				@Override
 				public boolean resolve() {
-					return super.resolve();
+					if (super.resolve()) {
+						if (getNormalY() > 0) jumpComponent.grounded = true;
+						return true;
+					} else return false;
 				}
 			};
 			broadPhaseCollisionComponent.collisions.add(collision);
