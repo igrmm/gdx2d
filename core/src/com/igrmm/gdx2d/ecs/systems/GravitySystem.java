e65912e0fef974c25df59dc1500d03212e7bbad0
igrmm <igrmmdev@gmail.com>
2021-01-19 14:49:08 -0400

    add GravitySystem

---
+++ core/src/com/igrmm/gdx2d/ecs/systems/GravitySystem.java
@@ -0,0 +1,24 @@
+package com.igrmm.gdx2d.ecs.systems;
+
+import com.igrmm.gdx2d.ecs.EntityManager;
+import com.igrmm.gdx2d.ecs.components.AccelerationComponent;
+import com.igrmm.gdx2d.ecs.components.GravityComponent;
+import com.igrmm.gdx2d.ecs.components.VelocityComponent;
+
+import java.util.Set;
+
+public class GravitySystem implements System {
+	@Override
+	public void update(EntityManager entityManager, float delta) {
+		Set<String> entities = entityManager.getAllEntitiesPossessingComponent(GravityComponent.class);
+		for (String entity : entities) {
+			AccelerationComponent accelerationComponent =
+					entityManager.getComponent(entity, AccelerationComponent.class);
+			VelocityComponent velocityComponent =
+					entityManager.getComponent(entity, VelocityComponent.class);
+
+			accelerationComponent.acceleration.y = GravityComponent.GRAVITY;
+			velocityComponent.velocity.y += accelerationComponent.acceleration.y;
+		}
+	}
+}
