9a927209f71330f79e69d7529633779a4a793259
igrmm <igrmmdev@gmail.com>
2021-01-21 10:10:11 -0400

    remove InputComponent.java

--- core/src/com/igrmm/gdx2d/ecs/components/InputComponent.java
+++
@@ -1,9 +0,0 @@
-package com.igrmm.gdx2d.ecs.components;
-
-public class InputComponent implements Component {
-	public boolean right = false;
-	public boolean left = false;
-	public boolean up = false;
-	public boolean down = false;
-	public boolean jump = false;
-}
