ae8385bc6c4ecff824534d5d2a7872a12c2533fb
igrmm <igrmmdev@gmail.com>
2021-01-21 22:00:20 -0400

    rewrite PlayerAnimationComponent for better readability

--- core/src/com/igrmm/gdx2d/ecs/components/PlayerAnimationComponent.java
+++ core/src/com/igrmm/gdx2d/ecs/components/PlayerAnimationComponent.java
@@ -10,45 +10,70 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PlayerAnimationComponent implements Component {
-
-	public enum PlayerAnimation {IDLE, WALK_RIGHT, WALK_LEFT}
-
 	public static final float SPRITE_OFFSET = 16.0f;
 
-	private final Map<PlayerAnimation, Animation<TextureRegion>> animations;
-	private PlayerAnimation playerAnimation;
+	private static final int SPRITE_SIZE = 64;
+	private static final int WALK_RIGHT = 0;
+	private static final int WALK_RIGHT_FRAMES = 2;
+	private static final int WALK_LEFT = 1;
+	private static final int WALK_LEFT_FRAMES = 2;
+	private static final int IDLE = 2;
+	private static final int IDLE_FRAMES = 1;
+
+	private int animation = IDLE;
+
 	private float stateTime = 0.0f;
 
+	private final Map<Integer, Animation<TextureRegion>> animations;
+
 	public PlayerAnimationComponent(Assets assets) {
-
 		animations = new HashMap<>();
-		playerAnimation = PlayerAnimation.IDLE;
 		Texture texture = assets.getTexture(TextureAsset.PLAYER);
-		TextureRegion[][] spriteSheet = TextureRegion.split(texture, 64, 64);
-		TextureRegion[] idle = new TextureRegion[1];
-		TextureRegion[] walkRight = new TextureRegion[2];
-		TextureRegion[] walkLeft = new TextureRegion[2];
+		TextureRegion[][] spriteSheet = TextureRegion.split(texture, SPRITE_SIZE, SPRITE_SIZE);
+		TextureRegion[] idle = new TextureRegion[IDLE_FRAMES];
+		TextureRegion[] walkRight = new TextureRegion[WALK_RIGHT_FRAMES];
+		TextureRegion[] walkLeft = new TextureRegion[WALK_LEFT_FRAMES];
 
-		idle[0] = spriteSheet[2][0];
-		for (int i = 0; i < 2; i++) {
-			walkRight[i] = spriteSheet[0][i];
-			walkLeft[i] = spriteSheet[1][i];
+		int maxFrames = texture.getWidth() / SPRITE_SIZE;
+		for (int i = 0; i < maxFrames; i++) {
+			if (i < WALK_RIGHT_FRAMES)
+				walkRight[i] = spriteSheet[WALK_RIGHT][i];
+
+			if (i < WALK_LEFT_FRAMES)
+				walkLeft[i] = spriteSheet[WALK_LEFT][i];
+
+			if (i < IDLE_FRAMES)
+				idle[i] = spriteSheet[IDLE][i];
 		}
 
-		animations.put(PlayerAnimation.IDLE, new Animation<>(1.0f, idle));
-		animations.put(PlayerAnimation.WALK_RIGHT, new Animation<>(0.1f, walkRight));
-		animations.put(PlayerAnimation.WALK_LEFT, new Animation<>(0.1f, walkLeft));
+		animations.put(IDLE, new Animation<>(1.0f, idle));
+		animations.put(WALK_RIGHT, new Animation<>(0.1f, walkRight));
+		animations.put(WALK_LEFT, new Animation<>(0.1f, walkLeft));
 	}
 
-	public void setAnimation(PlayerAnimation playerAnimation) {
-		if (this.playerAnimation != playerAnimation) {
-			this.playerAnimation = playerAnimation;
+	public void setWalkRightAnimation() {
+		if (animation != WALK_RIGHT) {
+			animation = WALK_RIGHT;
+			stateTime = 0.0f;
+		}
+	}
+
+	public void setWalkLeftAnimation() {
+		if (animation != WALK_LEFT) {
+			animation = WALK_LEFT;
+			stateTime = 0.0f;
+		}
+	}
+
+	public void setIdleAnimation() {
+		if (animation != IDLE) {
+			animation = IDLE;
 			stateTime = 0.0f;
 		}
 	}
 
 	public TextureRegion getKeyFrame(float delta) {
 		stateTime += delta;
-		return animations.get(playerAnimation).getKeyFrame(stateTime, true);
+		return animations.get(animation).getKeyFrame(stateTime, true);
 	}
 }
