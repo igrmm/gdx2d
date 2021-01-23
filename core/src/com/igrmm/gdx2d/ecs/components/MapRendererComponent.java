2e686dbde310c1bdfe0d61a5c04f157770eeeb3d
igrmm <igrmmdev@gmail.com>
2021-01-23 15:08:17 -0400

    MapRendererComponent has TiledMap as dependency and implements disposable

--- core/src/com/igrmm/gdx2d/ecs/components/MapRendererComponent.java
+++ core/src/com/igrmm/gdx2d/ecs/components/MapRendererComponent.java
@@ -1,11 +1,18 @@
 package com.igrmm.gdx2d.ecs.components;
 
+import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.Disposable;
 
-public class MapRendererComponent implements Component {
+public class MapRendererComponent implements Component, Disposable {
 	public final OrthogonalTiledMapRenderer mapRenderer;
 
-	public MapRendererComponent(OrthogonalTiledMapRenderer mapRenderer) {
-		this.mapRenderer = mapRenderer;
+	public MapRendererComponent(TiledMap tiledMap) {
+		this.mapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+	}
+
+	@Override
+	public void dispose() {
+		mapRenderer.dispose();
 	}
 }
