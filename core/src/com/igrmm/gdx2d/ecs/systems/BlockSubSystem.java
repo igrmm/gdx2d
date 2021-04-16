72c45ca15699bd46c059223e74c61523dc32f388
igrmm <igrmmdev@gmail.com>
2021-04-16 11:54:53 -0400

    adopt new naming convention in BlockSubSystem

--- core/src/com/igrmm/gdx2d/ecs/systems/BlockSubSystem.java
+++ core/src/com/igrmm/gdx2d/ecs/systems/BlockSubSystem.java
@@ -1,6 +1,7 @@
 package com.igrmm.gdx2d.ecs.systems;
 
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.igrmm.gdx2d.ecs.Collision;
 import com.igrmm.gdx2d.ecs.EntityManager;
 import com.igrmm.gdx2d.ecs.components.*;
@@ -11,45 +12,51 @@ import java.util.Set;
 public class BlockSubSystem implements SubSystem {
 	@Override
 	public void update(EntityManager entityManager, float delta) {
+		Set<String> entitiesPossessingBBoxC =
+				entityManager.getAllEntitiesPossessingComponent(BoundingBoxComponent.class);
+		Set<String> entitiesPossessingVelC =
+				entityManager.getAllEntitiesPossessingComponent(VelocityComponent.class);
+		Set<String> entitiesPossessingJumpC =
+				entityManager.getAllEntitiesPossessingComponent(JumpComponent.class);
 
-		Set<String> blocks = entityManager.getAllEntitiesPossessingComponent(BoundingBoxComponent.class);
-		Set<String> mobiles = entityManager.getAllEntitiesPossessingComponent(VelocityComponent.class);
-		Set<String> jumpers = entityManager.getAllEntitiesPossessingComponent(JumpComponent.class);
+		for (String entityPossessingBBoxC : entitiesPossessingBBoxC) {
+			TypeComponent typeC =
+					entityManager.getComponent(entityPossessingBBoxC, TypeComponent.class);
+			EntityType entityType = typeC.type;
 
-		for (String block : blocks) {
-			TypeComponent typeComponent = entityManager.getComponent(block, TypeComponent.class);
-			if (typeComponent.type == EntityType.BLOCK) {
-				BoundingBoxComponent staticBBoxComponent =
-						entityManager.getComponent(block, BoundingBoxComponent.class);
+			if (entityType == EntityType.BLOCK) {
+				BoundingBoxComponent staticBBoxC =
+						entityManager.getComponent(entityPossessingBBoxC, BoundingBoxComponent.class);
 
-				for (String mobile : mobiles) {
-					BoundingBoxComponent dynamicBBoxComponent =
-							entityManager.getComponent(mobile, BoundingBoxComponent.class);
-					VelocityComponent velocityComponent =
-							entityManager.getComponent(mobile, VelocityComponent.class);
-					BroadPhaseCollisionComponent broadPhaseCollisionComponent =
-							entityManager.getComponent(mobile, BroadPhaseCollisionComponent.class);
+				for (String entityPossessingVelC : entitiesPossessingVelC) {
+					BoundingBoxComponent dynamicBBoxC =
+							entityManager.getComponent(entityPossessingVelC, BoundingBoxComponent.class);
+					VelocityComponent velocityC =
+							entityManager.getComponent(entityPossessingVelC, VelocityComponent.class);
+					BroadPhaseCollisionComponent broadPhaseCollisionC =
+							entityManager.getComponent(entityPossessingVelC, BroadPhaseCollisionComponent.class);
 
-					Rectangle dynamicBBox = dynamicBBoxComponent.bBox;
-					Rectangle staticBBox = staticBBoxComponent.bBox;
+					Rectangle dynamicBBox = dynamicBBoxC.bBox;
+					Rectangle staticBBox = staticBBoxC.bBox;
+					Vector2 velocity = velocityC.velocity;
 
 					Collision collision =
-							new Collision(dynamicBBox, staticBBox, velocityComponent.velocity) {
+							new Collision(dynamicBBox, staticBBox, velocity) {
 								@Override
 								public boolean resolve() {
 									if (super.resolve()) {
 										if (getNormalY() > 0) {
-											if (jumpers.contains(mobile)) {
-												JumpComponent jumpComponent =
-														entityManager.getComponent(mobile, JumpComponent.class);
-												jumpComponent.grounded = true;
+											if (entitiesPossessingJumpC.contains(entityPossessingVelC)) {
+												JumpComponent jumpC =
+														entityManager.getComponent(entityPossessingVelC, JumpComponent.class);
+												jumpC.grounded = true;
 											}
 										}
 										return true;
 									} else return false;
 								}
 							};
-					broadPhaseCollisionComponent.collisions.add(collision);
+					broadPhaseCollisionC.collisions.add(collision);
 				}
 			}
 		}
