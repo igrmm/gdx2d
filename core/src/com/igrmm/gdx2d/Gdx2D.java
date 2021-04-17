056e80e808926db03d2447ecce47ad9509f94567
igrmm <igrmmdev@gmail.com>
2021-04-17 16:58:52 -0400

    add temporary mapAsset field in Gdx2d

--- core/src/com/igrmm/gdx2d/Gdx2D.java
+++ core/src/com/igrmm/gdx2d/Gdx2D.java
@@ -1,12 +1,14 @@
 package com.igrmm.gdx2d;
 
 import com.badlogic.gdx.Game;
+import com.igrmm.gdx2d.enums.MapAsset;
 import com.igrmm.gdx2d.screens.GameScreen;
 import com.igrmm.gdx2d.screens.LoadScreen;
 
 public class Gdx2D extends Game {
 	public final Assets assets = new Assets();
 	private GameScreen gameScreen;
+	public MapAsset mapAsset = MapAsset.START;
 
 	@Override
 	public void create() {
