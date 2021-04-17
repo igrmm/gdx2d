15ac30af36f961ba2c88fa4ab312f713bc888cb6
igrmm <igrmmdev@gmail.com>
2021-04-17 16:52:23 -0400

    BatchComponent implements disposable interface

--- core/src/com/igrmm/gdx2d/ecs/components/BatchComponent.java
+++ core/src/com/igrmm/gdx2d/ecs/components/BatchComponent.java
@@ -1,7 +1,15 @@
 package com.igrmm.gdx2d.ecs.components;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Disposable;
 
-public class BatchComponent implements Component {
+public class BatchComponent implements Component, Disposable {
+	public boolean dispose = false;
 	public final SpriteBatch batch = new SpriteBatch();
+
+	@Override
+	public void dispose() {
+		dispose = true;
+		batch.dispose();
+	}
 }
