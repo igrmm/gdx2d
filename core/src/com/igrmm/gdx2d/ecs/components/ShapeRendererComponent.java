7c0b9337def8c9df96af9442f672f77622b06e80
igrmm <igrmmdev@gmail.com>
2021-04-17 16:51:38 -0400

    ShapeRendererComponent implements disposable interface

--- core/src/com/igrmm/gdx2d/ecs/components/ShapeRendererComponent.java
+++ core/src/com/igrmm/gdx2d/ecs/components/ShapeRendererComponent.java
@@ -1,7 +1,15 @@
 package com.igrmm.gdx2d.ecs.components;
 
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.Disposable;
 
-public class ShapeRendererComponent implements Component {
+public class ShapeRendererComponent implements Component, Disposable {
+	public boolean dispose = false;
 	public final ShapeRenderer shapeRenderer = new ShapeRenderer();
+
+	@Override
+	public void dispose() {
+		dispose = true;
+		shapeRenderer.dispose();
+	}
 }
