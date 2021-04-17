726f143a5404419c46ec851951aa95889af8a6c3
igrmm <igrmmdev@gmail.com>
2021-04-17 16:53:07 -0400

    dispose resources using list

--- core/src/com/igrmm/gdx2d/screens/GameScreen.java
+++ core/src/com/igrmm/gdx2d/screens/GameScreen.java
@@ -1,9 +1,9 @@
 package com.igrmm.gdx2d.screens;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.utils.Disposable;
 import com.igrmm.gdx2d.Gdx2D;
 import com.igrmm.gdx2d.ecs.EntityManager;
-import com.igrmm.gdx2d.ecs.components.*;
 import com.igrmm.gdx2d.ecs.systems.*;
 import com.igrmm.gdx2d.ecs.systems.SubSystem;
 
@@ -12,31 +12,22 @@ import java.util.*;
 public class GameScreen extends ScreenAdapter {
 	private final EntityManager entityManager;
 	private final LinkedHashSet<SubSystem> subSystems;
+	private final List<Disposable> disposables;
 
 	public GameScreen(Gdx2D game) {
 		entityManager = new EntityManager();
 		subSystems = new LinkedHashSet<>();
-		subSystems.add(new InitializeSubSystem(game, subSystems));
+		disposables = new ArrayList<>();
+		subSystems.add(new InitializeSubSystem(game, subSystems, disposables));
 	}
 
 	@Override
 	public void render(float delta) {
-		for (SubSystem subSystem : subSystems)
-			subSystem.update(entityManager, delta);
+		for (SubSystem subSystem : subSystems) subSystem.update(entityManager, delta);
 	}
 
 	@Override
 	public void dispose() {
-		String coreUUID = entityManager.coreUUID;
-		BatchComponent batchC =
-				entityManager.getComponent(coreUUID, BatchComponent.class);
-		MapRendererComponent mapRendererC =
-				entityManager.getComponent(coreUUID, MapRendererComponent.class);
-		ShapeRendererComponent shapeRendererC =
-				entityManager.getComponent(coreUUID, ShapeRendererComponent.class);
-
-		batchC.batch.dispose();
-		mapRendererC.mapRenderer.dispose();
-		shapeRendererC.shapeRenderer.dispose();
+		for (Disposable disposable : disposables) disposable.dispose();
 	}
 }
