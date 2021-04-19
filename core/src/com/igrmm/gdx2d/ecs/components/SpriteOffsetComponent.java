9409966888ce64e84180ba23a4a2b70c37ed4b75
igrmm <igrmmdev@gmail.com>
2021-04-19 14:30:15 -0400

    add SpriteOffsetComponent

---
+++ core/src/com/igrmm/gdx2d/ecs/components/SpriteOffsetComponent.java
@@ -0,0 +1,9 @@
+package com.igrmm.gdx2d.ecs.components;
+
+public class SpriteOffsetComponent implements Component {
+	public final float spriteOffset;
+
+	public SpriteOffsetComponent(float spriteOffset) {
+		this.spriteOffset = spriteOffset;
+	}
+}
