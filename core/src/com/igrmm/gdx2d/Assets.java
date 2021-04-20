1d1e53ed206a45f83df11e1f8df3274d31f1275a
igrmm <igrmmdev@gmail.com>
2021-04-20 13:48:26 -0400

    add animation data to assets class

--- core/src/com/igrmm/gdx2d/Assets.java
+++ core/src/com/igrmm/gdx2d/Assets.java
@@ -1,19 +1,29 @@
 package com.igrmm.gdx2d;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+import com.igrmm.gdx2d.ecs.AnimationData;
+import com.igrmm.gdx2d.enums.AnimationAsset;
 import com.igrmm.gdx2d.enums.MapAsset;
 import com.igrmm.gdx2d.enums.TextureAsset;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Assets {
 	private final AssetManager assetManager;
+	private final Map<AnimationAsset, AnimationData> animationData;
 
 	public Assets() {
 		assetManager = new AssetManager();
 		assetManager.setLoader(TiledMap.class, new TmxMapLoader(new InternalFileHandleResolver()));
+		animationData = new HashMap<>();
 	}
 
 	public void load() {
@@ -26,14 +36,23 @@ public class Assets {
 		}
 
 		assetManager.finishLoading();
+
+		for (AnimationAsset animationAsset : AnimationAsset.values()) {
+			JsonValue animationJson = new JsonReader().parse(Gdx.files.internal(animationAsset.getPath()));
+			String texturePath = "images/" + animationJson.get("meta").getString("image");
+			if (TextureAsset.contains(texturePath)) {
+				Texture animationTexture = assetManager.get(texturePath, Texture.class);
+				animationData.put(animationAsset, new AnimationData(animationJson, animationTexture));
+			} else throw new NullPointerException("wrong texture path: " + texturePath);
+		}
 	}
 
 	public TiledMap getTiledMap(MapAsset mapAsset) {
 		return assetManager.get(mapAsset.getPath());
 	}
 
-	public Texture getTexture(TextureAsset textureAsset) {
-		return assetManager.get(textureAsset.getPath());
+	public AnimationData getAnimationData(AnimationAsset animationAsset) {
+		return animationData.get(animationAsset);
 	}
 
 	public void dispose() {
