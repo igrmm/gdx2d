7bae7b0de03300bd89f0cd699a111e98de89b6ef
igrmm <igrmmdev@gmail.com>
2021-04-20 13:47:13 -0400

    add reverse lookup and contains method in TextureAsset enum

--- core/src/com/igrmm/gdx2d/enums/TextureAsset.java
+++ core/src/com/igrmm/gdx2d/enums/TextureAsset.java
@@ -1,8 +1,20 @@
 package com.igrmm.gdx2d.enums;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum TextureAsset {
 	PLAYER("images/player.png"),
 	PORTAL("images/portal.png");
+
+	// Reverse-lookup map for getting a texture asset from an its path
+	private static final Map<String, TextureAsset> lookup = new HashMap<>();
+
+	static {
+		for (TextureAsset textureAsset : TextureAsset.values()) {
+			lookup.put(textureAsset.getPath(), textureAsset);
+		}
+	}
 
 	private final String path;
 
@@ -12,6 +24,22 @@ public enum TextureAsset {
 
 	public String getPath() {
 		return path;
+	}
+
+	public static TextureAsset get(String path) {
+		TextureAsset textureAsset = lookup.get(path);
+		if (textureAsset != null) {
+			return textureAsset;
+		} else {
+			throw new NullPointerException("wrong texture path: " + path);
+		}
+	}
+
+	public static boolean contains(String path) {
+		for (TextureAsset textureAsset : TextureAsset.values()) {
+			if (path.equals(textureAsset.getPath())) return true;
+		}
+		return false;
 	}
 }
 
