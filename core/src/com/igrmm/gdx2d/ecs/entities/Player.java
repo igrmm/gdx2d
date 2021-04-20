56e389a5c43fdf4ac5a9e20a586c204887b4b742
igrmm <igrmmdev@gmail.com>
2021-04-20 13:49:08 -0400

    make player use new animation component

--- core/src/com/igrmm/gdx2d/ecs/entities/Player.java
+++ core/src/com/igrmm/gdx2d/ecs/entities/Player.java
@@ -3,14 +3,15 @@ package com.igrmm.gdx2d.ecs.entities;
 import com.igrmm.gdx2d.Assets;
 import com.igrmm.gdx2d.ecs.EntityManager;
 import com.igrmm.gdx2d.ecs.components.*;
+import com.igrmm.gdx2d.enums.AnimationAsset;
 import com.igrmm.gdx2d.enums.EntityType;
 
 public class Player {
 	public static void spawn(Assets assets, EntityManager entityManager) {
 		String playerUUID = entityManager.playerUUID;
 		entityManager.addComponent(playerUUID, new TypeComponent(EntityType.PLAYER));
-		entityManager.addComponent(playerUUID, new PlayerAnimationComponent(assets));
-		entityManager.addComponent(playerUUID, new KeyFrameComponent(PlayerAnimationComponent.SPRITE_OFFSET));
+		entityManager.addComponent(playerUUID, new AnimationComponent(assets.getAnimationData(AnimationAsset.PLAYER)));
+		entityManager.addComponent(playerUUID, new SpriteOffsetComponent(16.0f));
 		entityManager.addComponent(playerUUID, new BoundingBoxComponent(100.0f, 100.0f, 32.0f, 32.0f));
 		VelocityComponent velocityComponent = new VelocityComponent();
 		velocityComponent.maxVelocity.set(4.0f, 4.0f);
