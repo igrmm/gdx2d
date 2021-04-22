99e4482de2ad05dd40a6778a534eb4fdcb6ec8ae
igrmm <igrmmdev@gmail.com>
2021-04-22 19:59:12 -0400

    make font public

--- core/src/com/igrmm/gdx2d/ecs/components/FontComponent.java
+++ core/src/com/igrmm/gdx2d/ecs/components/FontComponent.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.utils.Disposable;
 
 public class FontComponent implements Component, Disposable {
-	private final BitmapFont font = new BitmapFont();
+	public final BitmapFont font = new BitmapFont();
 
 	@Override
 	public void dispose() {
