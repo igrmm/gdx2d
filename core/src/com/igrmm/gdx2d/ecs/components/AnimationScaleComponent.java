95c39861ba665316d844219b5600e970f9340804
igrmm <igrmmdev@gmail.com>
2021-05-14 16:41:55 -0400

    add AnimationScaleComponent

---
+++ core/src/com/igrmm/gdx2d/ecs/components/AnimationScaleComponent.java
@@ -0,0 +1,9 @@
+package com.igrmm.gdx2d.ecs.components;
+
+public class AnimationScaleComponent implements Component {
+	public final float scale;
+
+	public AnimationScaleComponent(float scale) {
+		this.scale = scale;
+	}
+}
