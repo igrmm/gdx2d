7382d2c0e93415d60b705fb940abc78d97e6515d
igrmm <igrmmdev@gmail.com>
2021-05-14 18:42:50 -0400

    add AnimationRotationComponent

---
+++ core/src/com/igrmm/gdx2d/ecs/components/AnimationRotationComponent.java
@@ -0,0 +1,9 @@
+package com.igrmm.gdx2d.ecs.components;
+
+public class AnimationRotationComponent implements Component {
+	public final float rotation;
+
+	public AnimationRotationComponent(float rotation) {
+		this.rotation = rotation;
+	}
+}
