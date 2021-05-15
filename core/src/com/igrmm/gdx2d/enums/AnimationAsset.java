69325abfce554e3d6bbbdf161c9a17b0c62f6960
igrmm <igrmmdev@gmail.com>
2021-05-14 21:56:29 -0400

    split virtual game pad art

--- core/src/com/igrmm/gdx2d/enums/AnimationAsset.java
+++ core/src/com/igrmm/gdx2d/enums/AnimationAsset.java
@@ -3,7 +3,11 @@ package com.igrmm.gdx2d.enums;
 public enum AnimationAsset {
 	PLAYER("animations/player.json"),
 	PORTAL("animations/portal.json"),
-	BULLET("animations/bullet.json");
+	BULLET("animations/bullet.json"),
+	VIRTUAL_LEFT_BUTTON("animations/virtual_left_button.json"),
+	VIRTUAL_RIGHT_BUTTON("animations/virtual_right_button.json"),
+	VIRTUAL_A_BUTTON("animations/virtual_a_button.json"),
+	VIRTUAL_B_BUTTON("animations/virtual_b_button.json");
 
 	private final String path;
 
