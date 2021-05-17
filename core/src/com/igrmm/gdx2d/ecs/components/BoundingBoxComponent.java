d0c981267fb036240ff1e8c6756d09ae9132f4bb
igrmm <igrmmdev@gmail.com>
2021-05-17 16:16:01 -0400

    add empty constructor in BoundingBoxComponent class

--- core/src/com/igrmm/gdx2d/ecs/components/BoundingBoxComponent.java
+++ core/src/com/igrmm/gdx2d/ecs/components/BoundingBoxComponent.java
@@ -5,6 +5,10 @@ import com.badlogic.gdx.math.Rectangle;
 public class BoundingBoxComponent implements Component {
 	public final Rectangle bBox;
 
+	public BoundingBoxComponent() {
+		bBox = new Rectangle();
+	}
+
 	public BoundingBoxComponent(float x, float y, float width, float height) {
 		bBox = new Rectangle(x, y, width, height);
 	}
