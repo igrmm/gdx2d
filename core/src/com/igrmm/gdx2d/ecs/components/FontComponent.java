d53df9af2b32100bcb7b603a3b751850b039761f
igrmm <igrmmdev@gmail.com>
2021-05-19 16:05:52 -0400

    add FontComponent

--- core/src/com/igrmm/gdx2d/ecs/components/FontComponent.java
+++ core/src/com/igrmm/gdx2d/ecs/components/FontComponent.java
@@ -6,6 +6,10 @@ import com.badlogic.gdx.utils.Disposable;
 public class FontComponent implements Component, Disposable {
 	public final BitmapFont font = new BitmapFont();
 
+	public FontComponent() {
+		font.getData().setScale(3.0f);
+	}
+
 	@Override
 	public void dispose() {
 		font.dispose();
