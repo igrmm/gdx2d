8e6584b15f8bb5fbe8a26b4528e62dbcc30fb9c9
igrmm <igrmmdev@gmail.com>
2021-05-19 16:30:20 -0400

    display fps in DebugSubSystem

--- core/src/com/igrmm/gdx2d/ecs/systems/DebugSubSystem.java
+++ core/src/com/igrmm/gdx2d/ecs/systems/DebugSubSystem.java
@@ -6,9 +6,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.igrmm.gdx2d.ecs.EntityManager;
-import com.igrmm.gdx2d.ecs.components.BoundingBoxComponent;
-import com.igrmm.gdx2d.ecs.components.CameraComponent;
-import com.igrmm.gdx2d.ecs.components.ShapeRendererComponent;
+import com.igrmm.gdx2d.ecs.components.*;
 
 public class DebugSubSystem implements SubSystem {
 	@Override
@@ -19,22 +17,37 @@ public class DebugSubSystem implements SubSystem {
 				entityManager.getComponent(coreUUID, ShapeRendererComponent.class);
 		CameraComponent cameraC =
 				entityManager.getComponent(coreUUID, CameraComponent.class);
+		BatchComponent batchC =
+				entityManager.getComponent(coreUUID, BatchComponent.class);
+		FontComponent fontC =
+				entityManager.getComponent(coreUUID, FontComponent.class);
+
+		if (!batchC.dispose) {
+			batchC.batch.begin();
+			fontC.font.draw(
+					batchC.batch,
+					"FPS: " + Gdx.graphics.getFramesPerSecond(),
+					cameraC.camera.position.x - Gdx.graphics.getWidth() / 2.0f,
+					cameraC.camera.position.y + Gdx.graphics.getHeight() / 2.0f
+			);
+			batchC.batch.end();
+		}
 
 		if (!shapeRendererC.dispose) {
-			ShapeRenderer shapeRenderer = shapeRendererC.shapeRenderer;
-			OrthographicCamera camera = cameraC.camera;
-
-			String playerUUID = entityManager.playerUUID;
-			BoundingBoxComponent playerBBoxC =
-					entityManager.getComponent(playerUUID, BoundingBoxComponent.class);
-
-			Rectangle playerBBox = playerBBoxC.bBox;
-
-			shapeRenderer.setProjectionMatrix(camera.combined);
-			shapeRenderer.setColor(Color.RED);
-			shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-			shapeRenderer.rect(playerBBox.x, playerBBox.y, playerBBox.width, playerBBox.height);
-			shapeRenderer.end();
+//			ShapeRenderer shapeRenderer = shapeRendererC.shapeRenderer;
+//			OrthographicCamera camera = cameraC.camera;
+//
+//			String playerUUID = entityManager.playerUUID;
+//			BoundingBoxComponent playerBBoxC =
+//					entityManager.getComponent(playerUUID, BoundingBoxComponent.class);
+//
+//			Rectangle playerBBox = playerBBoxC.bBox;
+//
+//			shapeRenderer.setProjectionMatrix(camera.combined);
+//			shapeRenderer.setColor(Color.RED);
+//			shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+//			shapeRenderer.rect(playerBBox.x, playerBBox.y, playerBBox.width, playerBBox.height);
+//			shapeRenderer.end();
 		}
 	}
 }
