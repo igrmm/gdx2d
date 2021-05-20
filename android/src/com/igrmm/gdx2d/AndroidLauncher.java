6d51eb9f8ae49bb43125c045009110006743082e
igrmm <igrmmdev@gmail.com>
2021-05-19 20:36:48 -0400

    use immersive mode

--- android/src/com/igrmm/gdx2d/AndroidLauncher.java
+++ android/src/com/igrmm/gdx2d/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.igrmm.gdx2d.Gdx2D;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useImmersiveMode = true;
 		initialize(new Gdx2D(), config);
 	}
 }
