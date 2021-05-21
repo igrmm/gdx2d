d8a27ca2d63db0cd2dae07bd09942d7726122a06
igrmm <igrmmdev@gmail.com>
2021-05-21 16:57:46 -0400

    add ProjectileSubSystem

---
+++ core/src/com/igrmm/gdx2d/ecs/systems/ProjectileSubSystem.java
@@ -0,0 +1,10 @@
+package com.igrmm.gdx2d.ecs.systems;
+
+import com.igrmm.gdx2d.ecs.EntityManager;
+
+public class ProjectileSubSystem implements SubSystem {
+	@Override
+	public void update(EntityManager entityManager, float delta) {
+
+	}
+}
