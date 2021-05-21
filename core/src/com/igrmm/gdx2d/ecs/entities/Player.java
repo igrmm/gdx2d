85ea102661912d31ac879953170bd96dcbffc7c9
igrmm <igrmmdev@gmail.com>
2021-05-20 23:04:51 -0400

    make player start at Waypoint.START

--- core/src/com/igrmm/gdx2d/ecs/entities/Player.java
+++ core/src/com/igrmm/gdx2d/ecs/entities/Player.java
@@ -5,6 +5,9 @@ import com.igrmm.gdx2d.ecs.EntityManager;
 import com.igrmm.gdx2d.ecs.components.*;
 import com.igrmm.gdx2d.enums.AnimationAsset;
 import com.igrmm.gdx2d.enums.EntityType;
+import com.igrmm.gdx2d.enums.Waypoint;
+
+import java.util.Set;
 
 public class Player {
 	public static void spawn(Assets assets, EntityManager entityManager) {
@@ -12,7 +15,8 @@ public class Player {
 		entityManager.addComponent(playerUUID, new TypeComponent(EntityType.PLAYER));
 		entityManager.addComponent(playerUUID, new AnimationComponent(assets.getAnimationData(AnimationAsset.PLAYER)));
 		entityManager.addComponent(playerUUID, new AnimationOffsetComponent(16.0f));
-		entityManager.addComponent(playerUUID, new BoundingBoxComponent(100.0f, 100.0f, 32.0f, 32.0f));
+		BoundingBoxComponent playerBBoxC = new BoundingBoxComponent(100.0f, 100.0f, 32.0f, 32.0f);
+		entityManager.addComponent(playerUUID, playerBBoxC);
 		VelocityComponent velocityComponent = new VelocityComponent();
 		velocityComponent.maxVelocity.set(4.0f, 4.0f);
 		entityManager.addComponent(playerUUID, velocityComponent);
@@ -20,5 +24,20 @@ public class Player {
 		entityManager.addComponent(playerUUID, new GravityComponent());
 		entityManager.addComponent(playerUUID, new JumpComponent());
 		entityManager.addComponent(playerUUID, new BroadPhaseCollisionComponent());
+
+		// Make player start at Waypoint.START
+		Set<String> entitiesPossessingWaypointC =
+				entityManager.getAllEntitiesPossessingComponent(WaypointComponent.class);
+
+		for (String entityPossessingWaypointC : entitiesPossessingWaypointC) {
+			WaypointComponent waypointC =
+					entityManager.getComponent(entityPossessingWaypointC, WaypointComponent.class);
+			if (waypointC.waypoint == Waypoint.START) {
+				BoundingBoxComponent waypointBBoxC =
+						entityManager.getComponent(entityPossessingWaypointC, BoundingBoxComponent.class);
+				playerBBoxC.bBox.setPosition(waypointBBoxC.bBox.x, waypointBBoxC.bBox.y);
+				break;
+			}
+		}
 	}
 }
