41065840db292f655ec6b22c894cf3459e235d04
igrmm <igrmmdev@gmail.com>
2021-05-23 15:09:28 -0400

    measure velocity in pixels per second

--- core/src/com/igrmm/gdx2d/ecs/systems/DebugSubSystem.java
+++ core/src/com/igrmm/gdx2d/ecs/systems/DebugSubSystem.java
@@ -9,6 +9,12 @@ import com.igrmm.gdx2d.ecs.EntityManager;
 import com.igrmm.gdx2d.ecs.components.*;
 
 public class DebugSubSystem implements SubSystem {
+	//measure velocity in pixels per second
+	private long velocityTime = System.currentTimeMillis();
+	private float averageVelocity = 0.0f;
+
+	private float playerLastX = 0.0f;
+
 	@Override
 	public void update(EntityManager entityManager, float delta) {
 		Gdx.graphics.setTitle(Gdx.graphics.getFramesPerSecond() + "");
@@ -25,11 +31,25 @@ public class DebugSubSystem implements SubSystem {
 		if (!batchC.dispose) {
 			fontC.font.getData().setScale(2.0f * cameraC.camera.zoom);
 
+			String playerUUID = entityManager.playerUUID;
+			BoundingBoxComponent playerBBoxC =
+					entityManager.getComponent(playerUUID, BoundingBoxComponent.class);
+
+			//measure velocity in pixels per second
+			if (System.currentTimeMillis() - velocityTime > 1000) {
+				averageVelocity =
+						Math.abs(playerBBoxC.bBox.x - playerLastX) / (System.currentTimeMillis() - velocityTime) * 1000;
+				averageVelocity = (float) Math.ceil(averageVelocity);
+				playerLastX = playerBBoxC.bBox.x;
+				velocityTime = System.currentTimeMillis();
+			}
+
 			//debug info
 			fontC.text = fontC.text.concat("FPS: " + Gdx.graphics.getFramesPerSecond() + "\n");
 			fontC.text = fontC.text.concat("DPI: " + Gdx.graphics.getDensity() + "\n");
 			fontC.text = fontC.text.concat("RES: " + Gdx.graphics.getWidth() + "x" + Gdx.graphics.getHeight() + "\n");
 			fontC.text = fontC.text.concat("CAMZOOM: " + cameraC.camera.zoom + "\n");
+			fontC.text = fontC.text.concat("AVERAGE VELOCITY: " + averageVelocity + "\n");
 
 			batchC.batch.begin();
 			fontC.font.draw(
