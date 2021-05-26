ab316c2b67745cc1710b7cedcae55971da2c1dce
igrmm <igrmmdev@gmail.com>
2021-05-25 20:47:06 -0400

    make velocity measurement into a inner class

--- core/src/com/igrmm/gdx2d/ecs/systems/DebugSubSystem.java
+++ core/src/com/igrmm/gdx2d/ecs/systems/DebugSubSystem.java
@@ -5,15 +5,12 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.igrmm.gdx2d.ecs.EntityManager;
 import com.igrmm.gdx2d.ecs.components.*;
 
 public class DebugSubSystem implements SubSystem {
-	//measure velocity in pixels per second
-	private long velocityTime = System.currentTimeMillis();
-	private float averageVelocity = 0.0f;
-
-	private float playerLastX = 0.0f;
+	private final AverageVelocity averageVelocity = new AverageVelocity();
 
 	@Override
 	public void update(EntityManager entityManager, float delta) {
@@ -35,21 +32,14 @@ public class DebugSubSystem implements SubSystem {
 			BoundingBoxComponent playerBBoxC =
 					entityManager.getComponent(playerUUID, BoundingBoxComponent.class);
 
-			//measure velocity in pixels per second
-			if (System.currentTimeMillis() - velocityTime > 1000) {
-				averageVelocity =
-						Math.abs(playerBBoxC.bBox.x - playerLastX) / (System.currentTimeMillis() - velocityTime) * 1000;
-				averageVelocity = (float) Math.ceil(averageVelocity);
-				playerLastX = playerBBoxC.bBox.x;
-				velocityTime = System.currentTimeMillis();
-			}
+			averageVelocity.update(playerBBoxC.bBox.x, playerBBoxC.bBox.y);
 
 			//debug info
 			fontC.text = fontC.text.concat("FPS: " + Gdx.graphics.getFramesPerSecond() + "\n");
 			fontC.text = fontC.text.concat("DPI: " + Gdx.graphics.getDensity() + "\n");
 			fontC.text = fontC.text.concat("RES: " + Gdx.graphics.getWidth() + "x" + Gdx.graphics.getHeight() + "\n");
 			fontC.text = fontC.text.concat("CAMZOOM: " + cameraC.camera.zoom + "\n");
-			fontC.text = fontC.text.concat("AVERAGE VELOCITY: " + averageVelocity + "\n");
+			fontC.text = fontC.text.concat("AVERAGE VELOCITY: " + averageVelocity.get() + "\n");
 
 			batchC.batch.begin();
 			fontC.font.draw(
@@ -77,6 +67,30 @@ public class DebugSubSystem implements SubSystem {
 //			shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
 //			shapeRenderer.rect(playerBBox.x, playerBBox.y, playerBBox.width, playerBBox.height);
 //			shapeRenderer.end();
+		}
+	}
+
+	private static final class AverageVelocity {
+		public long time0 = System.currentTimeMillis();
+		public Vector2 lastPosition = new Vector2();
+		public Vector2 averageVelocity = new Vector2();
+
+		public void update(float positionX, float positionY) {
+			long timeElapsed = System.currentTimeMillis() - time0;
+			if (timeElapsed > 1000) {
+				averageVelocity.x =
+						(int) (Math.abs(positionX - lastPosition.x) / (System.currentTimeMillis() - time0) * 1000);
+				lastPosition.x = positionX;
+				averageVelocity.y =
+						(int) (Math.abs(positionY - lastPosition.y) / (System.currentTimeMillis() - time0) * 1000);
+				lastPosition.y = positionY;
+
+				time0 = System.currentTimeMillis();
+			}
+		}
+
+		public Vector2 get() {
+			return averageVelocity;
 		}
 	}
 }
