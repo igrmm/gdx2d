c33cafd382472f7c607403cfa3194a0b99d4d5ff
igrmm <igrmmdev@gmail.com>
2021-06-01 22:37:29 -0400

    fix virtual button size (1.5 centimeters)

--- core/src/com/igrmm/gdx2d/ecs/systems/InitializeSubSystem.java
+++ core/src/com/igrmm/gdx2d/ecs/systems/InitializeSubSystem.java
@@ -72,81 +72,102 @@ public class InitializeSubSystem implements SubSystem {
 	}
 
 	private void spawnVirtualButtons(EntityManager entityManager) {
-		float SCALE = 5.0f * Gdx.graphics.getDensity();
+		//DPI STUFF
+		final float BUTTON_SIZE_CM = 1.5f;
+		final float BUTTON_SIZE_PX = 16.0f;
+		final float PIXEL_SIZE_CM = 2.0f / (Gdx.graphics.getPpcX() + Gdx.graphics.getPpcY());
+		final float SCALE = BUTTON_SIZE_CM / (BUTTON_SIZE_PX * PIXEL_SIZE_CM);
+
 		String initialAnimation = "up";
 		float screenWidth = Gdx.graphics.getWidth();
 		float screenHeight = Gdx.graphics.getHeight();
-		float PADDING = 3.0f * screenWidth / 100.0f;
+		final float PADDING = 3.0f * screenWidth / 100.0f;
 
 		/* LEFT BUTTON */
-		String vLeftBtnUUID = entityManager.virtualLeftButtonUUID;
-		entityManager.addComponent(vLeftBtnUUID, new TypeComponent(EntityType.VIRTUAL_LEFT_BUTTON));
-		entityManager.addComponent(vLeftBtnUUID, new AnimationScaleComponent(SCALE));
 		AnimationData vLeftBtnAnimationData = game.assets.getAnimationData(AnimationAsset.VIRTUAL_LEFT_BUTTON);
 		UIAnimationComponent vLeftBtnUIAnimationC = new UIAnimationComponent(vLeftBtnAnimationData, initialAnimation);
-		entityManager.addComponent(vLeftBtnUUID, vLeftBtnUIAnimationC);
 		TextureRegion vLeftBtnTex = vLeftBtnUIAnimationC.getKeyFrame(0.0f);
+		if (vLeftBtnTex.getRegionHeight() != BUTTON_SIZE_PX || vLeftBtnTex.getRegionWidth() != BUTTON_SIZE_PX) {
+			throw new IllegalStateException("Virtual button size is not regular.");
+		}
+
+		String vLeftBtnUUID = entityManager.virtualLeftButtonUUID;
+		entityManager.addComponent(vLeftBtnUUID, new TypeComponent(EntityType.VIRTUAL_LEFT_BUTTON));
+		entityManager.addComponent(vLeftBtnUUID, vLeftBtnUIAnimationC);
+		entityManager.addComponent(vLeftBtnUUID, new AnimationScaleComponent(SCALE));
 
 		//relative to camera position
 		BoundingBoxComponent vLeftBtnBBoxC = new BoundingBoxComponent(
 				screenWidth / 2.0f - PADDING,
 				screenHeight / 2.0f - PADDING,
-				vLeftBtnTex.getRegionWidth() * SCALE,
-				vLeftBtnTex.getRegionHeight() * SCALE
+				BUTTON_SIZE_PX * SCALE,
+				BUTTON_SIZE_PX * SCALE
 		);
 		entityManager.addComponent(vLeftBtnUUID, vLeftBtnBBoxC);
 
 		/* RIGHT BUTTON */
-		String vRightBtnUUID = entityManager.virtualRightButtonUUID;
-		entityManager.addComponent(vRightBtnUUID, new TypeComponent(EntityType.VIRTUAL_RIGHT_BUTTON));
-		entityManager.addComponent(vRightBtnUUID, new AnimationScaleComponent(SCALE));
 		AnimationData vRightBtnAnimationData = game.assets.getAnimationData(AnimationAsset.VIRTUAL_RIGHT_BUTTON);
 		UIAnimationComponent vRightBtnUIAnimationC = new UIAnimationComponent(vRightBtnAnimationData, initialAnimation);
-		entityManager.addComponent(vRightBtnUUID, vRightBtnUIAnimationC);
 		TextureRegion vRightBtnTex = vRightBtnUIAnimationC.getKeyFrame(0.0f);
+		if (vRightBtnTex.getRegionHeight() != BUTTON_SIZE_PX || vRightBtnTex.getRegionWidth() != BUTTON_SIZE_PX) {
+			throw new IllegalStateException("Virtual button size is not regular.");
+		}
+
+		String vRightBtnUUID = entityManager.virtualRightButtonUUID;
+		entityManager.addComponent(vRightBtnUUID, new TypeComponent(EntityType.VIRTUAL_RIGHT_BUTTON));
+		entityManager.addComponent(vRightBtnUUID, vRightBtnUIAnimationC);
+		entityManager.addComponent(vRightBtnUUID, new AnimationScaleComponent(SCALE));
 
 		//relative to camera position
 		BoundingBoxComponent vRightBtnBBoxC = new BoundingBoxComponent(
-				vLeftBtnBBoxC.bBox.x - vLeftBtnBBoxC.bBox.width - PADDING,
+				vLeftBtnBBoxC.bBox.x - BUTTON_SIZE_PX * SCALE - PADDING,
 				screenHeight / 2.0f - PADDING,
-				vRightBtnTex.getRegionWidth() * SCALE,
-				vRightBtnTex.getRegionHeight() * SCALE
+				BUTTON_SIZE_PX * SCALE,
+				BUTTON_SIZE_PX * SCALE
 		);
 		entityManager.addComponent(vRightBtnUUID, vRightBtnBBoxC);
 
 		/* B BUTTON */
-		String vBBtnUUID = entityManager.virtualBButtonUUID;
-		entityManager.addComponent(vBBtnUUID, new TypeComponent(EntityType.VIRTUAL_B_BUTTON));
-		entityManager.addComponent(vBBtnUUID, new AnimationScaleComponent(SCALE));
 		AnimationData vBBtnAnimationData = game.assets.getAnimationData(AnimationAsset.VIRTUAL_B_BUTTON);
 		UIAnimationComponent vBBtnUIAnimationC = new UIAnimationComponent(vBBtnAnimationData, initialAnimation);
-		entityManager.addComponent(vBBtnUUID, vBBtnUIAnimationC);
 		TextureRegion vBBtnTex = vBBtnUIAnimationC.getKeyFrame(0.0f);
+		if (vBBtnTex.getRegionHeight() != BUTTON_SIZE_PX || vBBtnTex.getRegionWidth() != BUTTON_SIZE_PX) {
+			throw new IllegalStateException("Virtual button size is not regular.");
+		}
+
+		String vBBtnUUID = entityManager.virtualBButtonUUID;
+		entityManager.addComponent(vBBtnUUID, new TypeComponent(EntityType.VIRTUAL_B_BUTTON));
+		entityManager.addComponent(vBBtnUUID, vBBtnUIAnimationC);
+		entityManager.addComponent(vBBtnUUID, new AnimationScaleComponent(SCALE));
 
 		//relative to camera position
 		BoundingBoxComponent vBBtnBBoxC = new BoundingBoxComponent(
-				vBBtnTex.getRegionWidth() * SCALE + PADDING - screenWidth / 2.0f,
+				BUTTON_SIZE_PX * SCALE + PADDING - screenWidth / 2.0f,
 				screenHeight / 2.0f - PADDING,
-				vBBtnTex.getRegionWidth() * SCALE,
-				vBBtnTex.getRegionHeight() * SCALE
+				BUTTON_SIZE_PX * SCALE,
+				BUTTON_SIZE_PX * SCALE
 		);
 		entityManager.addComponent(vBBtnUUID, vBBtnBBoxC);
 
 		/* A BUTTON */
+		AnimationData vABtnAnimationData = game.assets.getAnimationData(AnimationAsset.VIRTUAL_A_BUTTON);
+		UIAnimationComponent vABtnUIAnimationC = new UIAnimationComponent(vABtnAnimationData, initialAnimation);
+		TextureRegion vABtnTex = vABtnUIAnimationC.getKeyFrame(0.0f);
+		if (vABtnTex.getRegionHeight() != BUTTON_SIZE_PX || vABtnTex.getRegionWidth() != BUTTON_SIZE_PX) {
+			throw new IllegalStateException("Virtual button size is not regular.");
+		}
+
 		String vABtnUUID = entityManager.virtualAButtonUUID;
 		entityManager.addComponent(vABtnUUID, new TypeComponent(EntityType.VIRTUAL_A_BUTTON));
 		entityManager.addComponent(vABtnUUID, new AnimationScaleComponent(SCALE));
-		AnimationData vABtnAnimationData = game.assets.getAnimationData(AnimationAsset.VIRTUAL_A_BUTTON);
-		UIAnimationComponent vABtnUIAnimationC = new UIAnimationComponent(vABtnAnimationData, initialAnimation);
 		entityManager.addComponent(vABtnUUID, vABtnUIAnimationC);
-		TextureRegion vABtnTex = vABtnUIAnimationC.getKeyFrame(0.0f);
 
 		//relative to camera position
 		BoundingBoxComponent vABtnBBoxC = new BoundingBoxComponent(
-				vBBtnBBoxC.bBox.x + PADDING + vABtnTex.getRegionWidth() * SCALE,
+				vBBtnBBoxC.bBox.x + PADDING + BUTTON_SIZE_PX * SCALE,
 				screenHeight / 2.0f - PADDING,
-				vABtnTex.getRegionWidth() * SCALE,
-				vABtnTex.getRegionHeight() * SCALE
+				BUTTON_SIZE_PX * SCALE,
+				BUTTON_SIZE_PX * SCALE
 		);
 		entityManager.addComponent(vABtnUUID, vABtnBBoxC);
 	}
