a3d9844b78e79c587ad3ec44e148767946f4dcb1
igrmm <igrmmdev@gmail.com>
2021-06-01 22:13:31 -0400

    add constructors do AccelerationComponent

--- core/src/com/igrmm/gdx2d/ecs/components/AccelerationComponent.java
+++ core/src/com/igrmm/gdx2d/ecs/components/AccelerationComponent.java
@@ -3,5 +3,13 @@ package com.igrmm.gdx2d.ecs.components;
 import com.badlogic.gdx.math.Vector2;
 
 public class AccelerationComponent implements Component {
-	public Vector2 acceleration = new Vector2();
+	public final Vector2 acceleration;
+
+	public AccelerationComponent() {
+		acceleration = new Vector2();
+	}
+
+	public AccelerationComponent(float x, float y) {
+		acceleration = new Vector2(x, y);
+	}
 }
