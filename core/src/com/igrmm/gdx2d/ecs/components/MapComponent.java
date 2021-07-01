c31813793064447599b8836fdf16283d7cd95b5c
igrmm <igrmmdev@gmail.com>
2021-06-30 22:30:03 -0400

    MapComponent defaults to MapAsset.START

--- core/src/com/igrmm/gdx2d/ecs/components/MapComponent.java
+++ core/src/com/igrmm/gdx2d/ecs/components/MapComponent.java
@@ -5,6 +5,10 @@ import com.igrmm.gdx2d.enums.MapAsset;
 public class MapComponent implements Component {
 	public final MapAsset mapAsset;
 
+	public MapComponent() {
+		this(MapAsset.START);
+	}
+
 	public MapComponent(MapAsset mapAsset) {
 		this.mapAsset = mapAsset;
 	}
