06ab633a002e6a7a4a8a1453c04cafb1e5016970
igrmm <igrmmdev@gmail.com>
2021-07-18 15:42:46 -0400

    set default values for primitives in MovementComponent

--- core/src/com/igrmm/gdx2d/ecs/components/MovementComponent.java
+++ core/src/com/igrmm/gdx2d/ecs/components/MovementComponent.java
@@ -6,11 +6,11 @@ public class MovementComponent implements Component {
 	public static final int RIGHT_DIRECTION = 1;
 	public static final int LEFT_DIRECTION = -1;
 
-	public float maxSpeed = 3.0f;
-	public float acceleration = 0.3f;
-	public float friction = 0.2f;
-	public float gravity = -0.5f;
-	public float jumpForce = 10.0f;
+	public float maxSpeed = 0.0f;
+	public float acceleration = 0.0f;
+	public float friction = 0.0f;
+	public float gravity = 0.0f;
+	public float jumpForce = 0.0f;
 	public boolean grounded = false;
 	public boolean jumping = false;
 	public Vector2 speed = new Vector2();
