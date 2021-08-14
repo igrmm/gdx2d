7e022897ca5fe768b3cc9591103271277131f68b
igrmm <igrmmdev@gmail.com>
2021-08-14 14:48:19 -0400

    using lwjgl3 backend

--- desktop/src/com/igrmm/gdx2d/desktop/DesktopLauncher.java
+++ desktop/src/com/igrmm/gdx2d/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package com.igrmm.gdx2d.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.igrmm.gdx2d.Gdx2D;
 
 public class DesktopLauncher {
 	public static void main(String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Gdx2D(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new Gdx2D(), config);
 	}
 }
