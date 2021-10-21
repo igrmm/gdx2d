880fba9521263cbd83b1d854abfa15df482e142c
igrmm <igrmmdev@gmail.com>
2021-10-21 13:32:57 -0400

    remove UIAnimationComponent, using scene2dui now

--- core/src/com/igrmm/gdx2d/ecs/components/UIAnimationComponent.java
+++
@@ -1,9 +0,0 @@
-package com.igrmm.gdx2d.ecs.components;
-
-import com.igrmm.gdx2d.ecs.AnimationData;
-
-public class UIAnimationComponent extends AnimationComponent {
-	public UIAnimationComponent(AnimationData animationData, String animation) {
-		super(animationData, animation);
-	}
-}
