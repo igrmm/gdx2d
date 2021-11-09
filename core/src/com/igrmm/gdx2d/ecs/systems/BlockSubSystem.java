3b879e043a60bc69d2d900427e389b4f76149824
igrmm <igrmmdev@gmail.com>
2021-11-08 21:46:01 -0400

    optimize BlockSubSystem code

--- core/src/com/igrmm/gdx2d/ecs/systems/BlockSubSystem.java
+++ core/src/com/igrmm/gdx2d/ecs/systems/BlockSubSystem.java
@@ -1,20 +1,53 @@
 package com.igrmm.gdx2d.ecs.systems;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Pools;
 import com.igrmm.gdx2d.ecs.Collision;
 import com.igrmm.gdx2d.ecs.Manager;
 import com.igrmm.gdx2d.ecs.components.*;
 import com.igrmm.gdx2d.enums.EntityType;
 
-import java.util.Set;
+import java.util.*;
 
 public class BlockSubSystem implements SubSystem {
-	@Override
-	public void update(Manager manager, float delta) {
+	private final Rectangle collisionArea = new Rectangle();
+	private final Rectangle activeZone = new Rectangle();
+	private final Map<String, Rectangle> staticBBoxes = new HashMap<>();
+
+	private void computeCollisionArea(Rectangle mobileRect, Vector2 speed) {
+		if (speed.x > 0.0f) {
+			collisionArea.x = mobileRect.x;
+			collisionArea.width = mobileRect.width + speed.x;
+		} else {
+			collisionArea.x = mobileRect.x + speed.x;
+			collisionArea.width = mobileRect.width - speed.x;
+		}
+
+		if (speed.y > 0.0f) {
+			collisionArea.y = mobileRect.y;
+			collisionArea.height = mobileRect.height + speed.y;
+		} else {
+			collisionArea.y = mobileRect.y + speed.y;
+			collisionArea.height = mobileRect.height - speed.y;
+		}
+	}
+
+	private void filterBoundingBoxes(Manager manager) {
 		Set<String> entitiesPossessingBBoxC =
 				manager.getAllEntitiesPossessingComponent(BoundingBoxComponent.class);
-		Set<String> entitiesPossessingMovC =
-				manager.getAllEntitiesPossessingComponent(MovementComponent.class);
+		BoundingBoxComponent playerBBoxC =
+				manager.getComponent(manager.playerUUID, BoundingBoxComponent.class);
+
+		staticBBoxes.clear();
+
+		activeZone.set(
+				playerBBoxC.bBox.x + playerBBoxC.bBox.width / 2.0f - Gdx.graphics.getWidth() / 2.0f,
+				playerBBoxC.bBox.y + playerBBoxC.bBox.height / 2.0f - Gdx.graphics.getHeight() / 2.0f,
+				Gdx.graphics.getWidth(),
+				Gdx.graphics.getHeight()
+		);
 
 		for (String entityPossessingBBoxC : entitiesPossessingBBoxC) {
 			TypeComponent typeC =
@@ -22,13 +55,41 @@ public class BlockSubSystem implements SubSystem {
 			EntityType entityType = typeC.type;
 
 			if (entityType == EntityType.BLOCK) {
-				for (String entityPossessingMovC : entitiesPossessingMovC) {
-					BroadPhaseCollisionComponent broadPhaseCollisionC =
-							manager.getComponent(entityPossessingMovC, BroadPhaseCollisionComponent.class);
+				BoundingBoxComponent staticBBoxC =
+						manager.getComponent(entityPossessingBBoxC, BoundingBoxComponent.class);
 
-					BlockCollision collision = Pools.obtain(BlockCollision.class);
-					collision.init(entityPossessingMovC, entityPossessingBBoxC, manager);
-					broadPhaseCollisionC.collisions.add(collision);
+				if (activeZone.overlaps(staticBBoxC.bBox))
+					staticBBoxes.put(entityPossessingBBoxC, staticBBoxC.bBox);
+			}
+		}
+	}
+
+	@Override
+	public void update(Manager manager, float delta) {
+		filterBoundingBoxes(manager);
+
+		Set<String> entitiesPossessingMovC =
+				manager.getAllEntitiesPossessingComponent(MovementComponent.class);
+
+		for (String entityPossessingMovC : entitiesPossessingMovC) {
+			MovementComponent movementC =
+					manager.getComponent(entityPossessingMovC, MovementComponent.class);
+			movementC.grounded = false;
+
+			if (movementC.speed.x != 0 || movementC.speed.y != 0) {
+				BroadPhaseCollisionComponent broadPhaseCollisionC =
+						manager.getComponent(entityPossessingMovC, BroadPhaseCollisionComponent.class);
+				BoundingBoxComponent dynamicBBoxC =
+						manager.getComponent(entityPossessingMovC, BoundingBoxComponent.class);
+				computeCollisionArea(dynamicBBoxC.bBox, movementC.speed);
+
+				for (String entityPossessingBBox : staticBBoxes.keySet()) {
+					Rectangle staticBBox = staticBBoxes.get(entityPossessingBBox);
+					if (collisionArea.overlaps(staticBBox)) {
+						BlockCollision collision = Pools.obtain(BlockCollision.class);
+						collision.init(entityPossessingMovC, entityPossessingBBox, manager);
+						broadPhaseCollisionC.collisions.add(collision);
+					}
 				}
 			}
 		}
@@ -41,7 +102,6 @@ public class BlockSubSystem implements SubSystem {
 		public void init(String dynamicEntity, String staticEntity, Manager manager) {
 			super.init(dynamicEntity, staticEntity, manager);
 			this.movementC = manager.getComponent(dynamicEntity, MovementComponent.class);
-			this.movementC.grounded = false;
 		}
 
 		@Override
